13bbd0b6f2271037bd691ffd2f490d488c2c378e
pekay23 <67763151+pekay23@users.noreply.github.com>
2026-02-03 16:29:58 +0000

    analytics and speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { AuthProvider } from "./providers";
 import MobileStickyBar from "@/components/MobileStickyBar";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Toaster } from 'sonner';
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -35,6 +37,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ScrollToTop />
         <MobileStickyBar />
         <Toaster richColors position="top-right" />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
