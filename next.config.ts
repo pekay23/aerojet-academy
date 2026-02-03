47db91351d193dec8106b08baf19cb54bfba44ab
pekay23 <67763151+pekay23@users.noreply.github.com>
2026-02-03 16:34:08 +0000

    env updates

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,29 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'cdn.sanity.io', port: '', pathname: '/**' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com', port: '', pathname: '/**' },
-      { protocol: 'https', hostname: 'utfs.io', port: '', pathname: '/**' }, 
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+        pathname: '/**',
+      },
     ],
+  },
+  // Optional: If you want to force the timezone to Africa/Accra for server-side dates
+  env: {
+    TZ: 'Africa/Accra',
   },
 };
 
