68db02a7a6a960659c7644e3f72cad0da637386e
pekay23 <67763151+pekay23@users.noreply.github.com>
2026-02-22 01:35:15 +0000

    prisma errors

--- lib/prisma/client.ts
+++ lib/prisma/client.ts
@@ -1,8 +1,22 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const connectionString = process.env.DATABASE_URL
+
+let prismaInstance: PrismaClient
+
+if (globalForPrisma.prisma) {
+  prismaInstance = globalForPrisma.prisma
+} else {
+  const pool = new Pool({ connectionString })
+  const adapter = new PrismaPg(pool)
+  prismaInstance = new PrismaClient({ adapter })
+}
+
+export const prisma = prismaInstance
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
