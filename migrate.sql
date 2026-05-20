82075890687a095a73354d4b773b3f87cc6b8f28
pekay23 <67763151+pekay23@users.noreply.github.com>
2026-05-20 23:54:17 +0000

    chore: remove temporary build scripts and legacy development files

--- migrate.sql
+++
@@ -1,7 +0,0 @@
-ALTER TYPE "ProgrammeChoice" ADD VALUE 'FLEXIBLE_COURSES';
-ALTER TYPE "EnrollmentType" ADD VALUE 'FLEXIBLE_COURSES';
-ALTER TYPE "StudyPathway" ADD VALUE 'FLEXIBLE_COURSES';
-
-UPDATE "users" SET "programmeChoice" = 'FLEXIBLE_COURSES' WHERE "programmeChoice" = 'MODULAR';
-UPDATE "student_profiles" SET "enrollmentType" = 'FLEXIBLE_COURSES' WHERE "enrollmentType" = 'MODULAR';
-UPDATE "student_profiles" SET "studyPathway" = 'FLEXIBLE_COURSES' WHERE "studyPathway" = 'MODULAR';
